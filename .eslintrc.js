037edf7991c772a2df6c07f1023faa6edd418325
vlallican <lallican.victorien35000@gmail.com>
2024-06-17 14:17:45 +0200

    lint and format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
-    "plugin:prettier/recommended",
-    "airbnb-base",
-    "airbnb-typescript/base",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    'plugin:prettier/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["./tsconfig.json"],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "class-methods-use-this": 0,
-    "comma-dangle": ["error", "never"],
-    "linebreak-style": 0,
-    "global-require": 0,
-    "eslint linebreak-style": [0, "error", "windows"],
-    "@typescript-eslint/comma-dangle": 0,
-    "no-new": 0,
-    "no-console": 0,
-  },
+    'class-methods-use-this': 0,
+    'comma-dangle': ['error', 'never'],
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    '@typescript-eslint/comma-dangle': 0,
+    'no-new': 0,
+    'no-console': 0
+  }
 };
