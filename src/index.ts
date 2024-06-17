4b48adf04a7435dc3d2104e5997c2b034c2fac57
Pierre Barbé <pierrebarbe0@gmail.com>
2024-06-17 15:31:01 +0200

    refactor: Update eslint configuration and remove unused rules

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,52 @@
+import Bot1 from './class/bot1';
+import { Bot3 } from './class/bot3';
 import './index.scss';
 
-const hello = 'hello test';
 
-console.log(hello);
-
-
-const trigger = document.getElementsByClassName('input-group-append');
-const input = document.getElementsByClassName('form-control')[0] as HTMLInputElement;
+const trigger = document.querySelector('.input-group-append') as HTMLElement;
+const input = document.querySelector('.form-control') as HTMLInputElement;
 const botSelector = document.getElementById('bot-selector') as HTMLSelectElement;
 const buttonSelector = document.getElementById('selector-button') as HTMLButtonElement;
 let selectedValue = 'bot1';
 
-if (trigger && botSelector) {
-    trigger[0].addEventListener('click', () => {
-        if (input.value === '') {
-            alert('Veuillez entrer une valeur valide');
-            return;
-        }
-        let inputValue = input.value;
-        console.log(input.value);
-        console.log(selectedValue);
-        //const bot = new Bot();
-        //const response = bot.messageAI(inputValue);
-        //console.log(response);
-    });
+const handleTriggerClick = () => {
+    if (!input.value.trim()) {
+        alert('Veuillez entrer une valeur valide');
+        return;
+    }
+    const inputValue = input.value;
+    addMessage(inputValue);
+
+    if (selectedValue === 'bot3') {
+        const bot3 = new Bot3();
+        bot3.onMessage(inputValue);
+    }
+    input.value = '';
+};
+
+const addMessage = (message: string): void => {
+    const chatContainer = document.getElementById('chat-messages');
+    if (!chatContainer) throw new Error('No chat messages container found');
+
+    chatContainer.innerHTML += `
+      <div class="message text-left">
+        <p><strong>Utilisateur</strong> <span class="user-message">${message}</span></p>
+      </div>
+    `;
+};
+
+const handleButtonClick = () => {
+    selectedValue = botSelector.value;
+    const chatMessages = document.getElementById('chat-messages');
+    if (chatMessages) {
+        chatMessages.innerHTML = '';
+    }
+};
+
+if (trigger) {
+    trigger.addEventListener('click', handleTriggerClick);
 }
 
 if (buttonSelector) {
-    buttonSelector.addEventListener('click', () => {
-        selectedValue = botSelector.value;
-        const chatMessages = document.getElementById('chat-messages');
-        if (chatMessages) {
-            chatMessages.innerHTML = '';
-        }
-    });
+    buttonSelector.addEventListener('click', handleButtonClick);
 }
