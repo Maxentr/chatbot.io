7b193d970f2e5f5a206e61ded01c4a9840deaca5
Pierre Barbé <pierrebarbe0@gmail.com>
2024-06-17 18:54:34 +0200

    refactor: Improve bot message handling and add support for Bot2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Bot1 from './class/bot1';
+import Bot2 from './class/bot2';
 import { Bot3 } from './class/bot3';
 import './index.scss';
+
 
 const trigger = document.querySelector('.input-group-append') as HTMLElement;
 const input = document.querySelector('.form-control') as HTMLInputElement;
@@ -8,44 +10,55 @@ const botSelector = document.getElementById('bot-selector') as HTMLSelectElement
 const buttonSelector = document.getElementById('selector-button') as HTMLButtonElement;
 let selectedValue = 'bot1';
 
-const handleTriggerClick = () => {
-  if (!input.value.trim()) {
-    alert('Veuillez entrer une valeur valide');
-    return;
-  }
-  const inputValue = input.value;
-  addMessage(inputValue);
 
-  if (selectedValue === 'bot3') {
-    const bot3 = new Bot3();
-    bot3.onMessage(inputValue);
-  }
-  input.value = '';
+const handleTriggerClick = () => {
+    if (!input.value.trim()) {
+        alert('Veuillez entrer une valeur valide');
+        return;
+    }
+    const inputValue = input.value;
+    addMessage(inputValue);
+
+    switch (selectedValue) {
+        case 'bot1':
+            new Bot1().onMessage(inputValue);
+            break;
+        case 'bot2':
+            new Bot2().onMessage(inputValue);
+            break;
+        case 'bot3':
+            new Bot3().onMessage(inputValue);
+            break;
+        default:
+            console.error(`Unknown bot type: ${inputValue}`);
+    }
+    input.value = '';
 };
 
 const addMessage = (message: string): void => {
-  const chatContainer = document.getElementById('chat-messages');
-  if (!chatContainer) throw new Error('No chat messages container found');
+    const chatContainer = document.getElementById('chat-messages');
+    if (!chatContainer) throw new Error('No chat messages container found');
 
-  chatContainer.innerHTML += `
+    chatContainer.innerHTML += `
       <div class="message text-left">
         <p><strong>Utilisateur</strong> <span class="user-message">${message}</span></p>
       </div>
     `;
 };
 
+
 const handleButtonClick = () => {
-  selectedValue = botSelector.value;
-  const chatMessages = document.getElementById('chat-messages');
-  if (chatMessages) {
-    chatMessages.innerHTML = '';
-  }
+    selectedValue = botSelector.value;
+    const chatMessages = document.getElementById('chat-messages');
+    if (chatMessages) {
+        chatMessages.innerHTML = '';
+    }
 };
 
 if (trigger) {
-  trigger.addEventListener('click', handleTriggerClick);
+    trigger.addEventListener('click', handleTriggerClick);
 }
 
 if (buttonSelector) {
-  buttonSelector.addEventListener('click', handleButtonClick);
+    buttonSelector.addEventListener('click', handleButtonClick);
 }
