217cc2c89c3ad7aec884bec365e61b12cc9e9007
Pierre Barbé <pierrebarbe0@gmail.com>
2024-06-17 14:46:01 +0200

    chore: Add event listeners for bot selector and trigger button

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,35 @@ import './index.scss';
 const hello = 'hello';
 
 console.log(hello);
+
+
+const trigger = document.getElementsByClassName('input-group-append');
+const input = document.getElementsByClassName('form-control')[0] as HTMLInputElement;
+const botSelector = document.getElementById('bot-selector') as HTMLSelectElement;
+const buttonSelector = document.getElementById('selector-button') as HTMLButtonElement;
+let selectedValue = 'bot1';
+
+if (trigger && botSelector) {
+    trigger[0].addEventListener('click', () => {
+        if (input.value === '') {
+            alert('Veuillez entrer une valeur valide');
+            return;
+        }
+        let inputValue = input.value;
+        console.log(input.value);
+        console.log(selectedValue);
+        //const bot = new Bot();
+        //const response = bot.messageAI(inputValue);
+        //console.log(response);
+    });
+}
+
+if (buttonSelector) {
+    buttonSelector.addEventListener('click', () => {
+        selectedValue = botSelector.value;
+        const chatMessages = document.getElementById('chat-messages');
+        if (chatMessages) {
+            chatMessages.innerHTML = '';
+        }
+    });
+}
