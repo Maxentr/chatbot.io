2e18280d4068c38d2774b029d9f8e13d4a926b8b
maxentr <rouaultmaxent@gmail.com>
2024-06-17 14:23:38 +0200

    fix(bot): name and description are not abstract

--- src/class/bot.ts
+++ src/class/bot.ts
@@ -5,20 +5,16 @@ const apiKey = 'API_KEY';
 const client = new MistralClient(apiKey);
 
 export default abstract class Bot {
-  abstract messages: string[];
-
-  abstract name: string;
-
-  abstract description: string;
-
+  name!: string;
+  description!: string;
+  
   public abstract getHelp(): string;
   public abstract onMessage(message: string): void;
-  public abstract sayWelcome(): void;
-
+  
   Bot(name: string, description: string) {
     this.name = name;
     this.description = description;
-  }
+    }
 
   protected addMessage(message: string): void {
     const chatContainer = document.getElementById('chat-messages');
