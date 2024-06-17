6fa708a2d47f333f180fe96fa79003a802cea1ff
maxentr <rouaultmaxent@gmail.com>
2024-06-17 11:57:58 +0200

    build: add prettier to format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,41 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
+    "plugin:prettier/recommended",
     "airbnb-base",
     "airbnb-typescript/base",
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
   ],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
       files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: "script"
-      }
-    }
+        sourceType: "script",
+      },
+    },
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    project: ["./tsconfig.json"]
+    project: ["./tsconfig.json"],
   },
   plugins: ["@typescript-eslint"],
   rules: {
     "class-methods-use-this": 0,
-    "comma-dangle": 0,
+    "comma-dangle": ["error", "never"],
     "linebreak-style": 0,
     "global-require": 0,
-    "eslint linebreak-style": 0,
+    "eslint linebreak-style": [0, "error", "windows"],
     "@typescript-eslint/comma-dangle": 0,
     "no-new": 0,
     "no-console": 0,
-  }
+  },
 };
